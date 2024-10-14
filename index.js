010880e13de36f3f5ce4019f37674a1719b4fab0
dariuskemei <dariuskimutai76@gmail.com>
2024-10-14 13:43:16 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const button = document.getElementById("button"); 
+
+    // Add an event listener for the 'click' event
+    button.addEventListener("click", function () {
+      console.log("Button clicked!");
+    });
 }
